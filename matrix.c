608480fde572ed8390ee6503339062142b101c9b
KARAN_KATAKDHOND <karankatakdhond23@gmail.com>
2024-06-20 12:37:48 +0530

    Hello Everyone

--- matrix.c
+++ matrix.c
@@ -3,7 +3,7 @@
 
 void main(){
 	int mat1[3][3],mat2[3][3];
-	printf("Enter Elements of Matrix 1: ");
+	printf("Enter Elemnts of Matrix 1: ");
 	for(int i=0;i<3;i++){
 		for(int j=0;j<3;j++){
 		printf("\ta%d%d = ",i,j);scanf("%d",&mat1[i][j]);
